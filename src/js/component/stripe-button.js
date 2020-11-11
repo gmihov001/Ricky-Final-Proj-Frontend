fd2bc2def7ed4773d7f1d940b62704d19bde5321
George Mihov <gmihov001@gmail.com>
2020-11-11 01:20:07 +0000

    Updates

--- src/js/component/stripe-button.js
+++ src/js/component/stripe-button.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
 import PropTypes from "prop-types";
+import { Context } from "../store/appContext";
 
 export const StripeCheckoutButton = ({ price }) => {
 	const { store, actions } = useContext(Context);
